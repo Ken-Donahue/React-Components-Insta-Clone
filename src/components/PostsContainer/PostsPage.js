9cd761c5769d5b90d1f1c8a32ff90a36f6636887
Ken Donahue <ken.w.donahue@gmail.com>
2020-04-14 19:04:49 -0400

    Posts are displaying, working on likes now

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,8 +1,9 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState}from "react";
 import Post from "./Post";
 import "./Posts.css";
+import dummyData from '../../dummy-data';
 // import data 
 
 const PostsPage = () => {
@@ -10,6 +11,9 @@ const PostsPage = () => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {
+      dummyData.map((post, index) => <Post post={post} key={index}/>)
+      }
     </div>
   );
 };
