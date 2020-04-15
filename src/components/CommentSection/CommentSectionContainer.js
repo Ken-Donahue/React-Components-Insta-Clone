35871c1909ad1439b85a8fe58a785be0636c00f8
Ken Donahue <ken.w.donahue@gmail.com>
2020-04-14 20:21:03 -0400

    Comments are now displaying correctly

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,13 +3,17 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import dummyData from '../../dummy-data'
 
+console.log(dummyData)
 const CommentSection = props => {
   // Add state for the comments
-
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {
+        props.comments.map((comment, index) => <Comment comment={comment} key ={index}/>)
+      }
       <CommentInput />
     </div>
   );
